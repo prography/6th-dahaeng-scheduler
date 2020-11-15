5f29d50e37e9f30fe1c9ce9c46c9b68f4d85cf37
Paikend <paikend@gmail.com>
2020-11-15 22:24:32 +0900

    Update messages_util.py

--- utils/messages_util.py
+++ utils/messages_util.py
@@ -1,17 +1,3 @@
-def set_push_notification_infos(user_info) -> Dict:
-    message = None
-    if user_info.is_written:
-        message = set_happy_words()
-    elif not user_info.is_written:
-        message = set_reminder()
-    else:
-        raise ValueError()
-    return {
-        "message": message,
-        "push_notification_token": user_info.push_notification_token,
-    }
-
-
 def set_happy_words() -> str:
     pass
 
