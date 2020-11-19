3779352c4f411d4be5740732cddbac68beafa9fc
Paikend <paikend@gmail.com>
2020-11-19 23:39:47 +0900

    Update push_notification_util.py

--- utils/push_notification_util.py
+++ utils/push_notification_util.py
@@ -1,13 +1,7 @@
-from utils.messages_util import set_happy_words, set_reminder
+from utils.messages_util import set_push_notification_message
 
 def set_push_notification_infos(user_info) -> Dict:
-    message = None
-    if user_info.is_written:
-        message = set_happy_words()
-    elif not user_info.is_written:
-        message = set_reminder()
-    else:
-        raise ValueError()
+    message = set_push_notification_message(user_info.is_written)
     return {
         "message": message,
         "push_notification_token": user_info.push_notification_token,
