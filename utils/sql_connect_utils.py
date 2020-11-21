77cb0eea6054ece2efac699ba5349925d7f7d74e
shunnyjang <suuuu101@naver.com>
2020-11-21 15:26:51 +0900

    Modify to seperate env_path from connect()

--- utils/sql_connect_utils.py
+++ utils/sql_connect_utils.py
@@ -5,22 +5,23 @@ from dotenv import load_dotenv
 from pathlib import Path  # Python 3.6+ only
 
 
+ENV_PATH = Path('.') / '.env'
+load_dotenv(dotenv_path=ENV_PATH)
+
+USER = os.getenv("DB_USER")
+PASSWORD = os.getenv("DB_PASSWORD")
+HOST = os.getenv("DB_HOST")
+PORT = os.getenv("DB_PORT")
+DB = os.getenv("DB_NAME")
+
+
 def connect():
     '''Returns a connection and a metadata object'''
     # We connect with the help of the PostgreSQL URL
     # postgresql://federer:grandestslam@localhost:5432/tennis
 
-    env_path = Path('.') / '.env'
-    load_dotenv(dotenv_path=env_path)
-
-    user = os.getenv("DB_USER")
-    password = os.getenv("DB_PASSWORD")
-    host = os.getenv("DB_HOST")
-    port = os.getenv("DB_PORT")
-    db = os.getenv("DB_NAME")
-
     url = 'postgresql://{}:{}@{}:{}/{}'
-    url = url.format(user, password, host, port, db)
+    url = url.format(USER, PASSWORD, HOST, PORT, DB)
 
     # The return value of create_engine() is our connection object
     con = sqlalchemy.create_engine(url, client_encoding='utf8')
